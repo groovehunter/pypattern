fd9a89c5017752827944c6aaaa165ae8dd5eed08
groovehunter <groovehunter@posteo.org>
2026-03-20 11:30:54 +0100

    connect as method for import

--- boot.py
+++ boot.py
@@ -1,6 +1,6 @@
 from wifikey import ESSID, PASS
 
-#import upip
+
 
 def connect():
     import network
@@ -8,18 +8,20 @@ def connect():
     if not sta_if.isconnected():
         print('connecting to network...')
         sta_if.active(True)
+        #sta_if.ifconfig(('192.168.43.10', '255.255.255.0', '192.168.43.1', '8.8.8.8'))
         sta_if.connect(ESSID, PASS)
-#        while not sta_if.isconnected():
-#            pass
-        for i in range(10000):
-            if sta_if.isconnected():
-                break
+        while not sta_if.isconnected():
+            pass
+#        for i in range(10000):
+#            if sta_if.isconnected():
+#                break
     print('network config:', sta_if.ifconfig())
 
 def installpip():
-  upip.install('micropython-uasyncio')
-  upip.install('micropython-pkg_resources')
+    import upip
+    upip.install('micropython-uasyncio')
+    upip.install('micropython-pkg_resources')
 
-
-connect()
+# for import only
+#connect()
 #installpip()
